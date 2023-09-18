e00c817ed2d199cf318d6f6b13e052c179356a33
sergei <ravinski.sergei@gmail.com>
2023-09-18 14:51:51 +0200

    Added variables

--- _app/js/modules/popup.js
+++ _app/js/modules/popup.js
@@ -1,4 +1,10 @@
 import FetchCountries from './fetch_countries.js';
 
 export default async function Popup() {
+	const countries = await FetchCountries();
+	const generateButton = document.querySelector('.main__button button');
+	const frontpage = document.querySelector('.main__frontpage');
+	const popup = document.querySelector('.main__popup');
+
+	let randomIndex = 0;
 }
